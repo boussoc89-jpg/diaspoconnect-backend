53143cd8de8b89c8f76f383426d7ef362f788f53
boussocisse <boussoc89@gmail.com>
2026-06-21 13:27:55 +0200

    fix: disable SSL cert verification for Aiven

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     dialect: "mysql",
     dialectOptions: {
       ssl: {
-        rejectUnauthorized: true, // Aiven exige un certificat valide
+        rejectUnauthorized: false, // Aiven exige un certificat valide
       },
     },
     logging: false,
